5df7d37d00827df02b132f697dae8ba3d3c2a480
BRomanchuk <47035610+BRomanchuk@users.noreply.github.com>
2021-08-21 22:53:17 +0300

    upd DataLoader

--- DataLoader.py
+++ DataLoader.py
@@ -2,5 +2,45 @@ import numpy as np
 import pandas as pd
 
 import gspread
-from config.constants import PROGRAMS_SHEET, PROFESSORS_SHEET, MANAGERS_SHEET, SERVICE_SHEET, SCHEDULE_SHEET
+from oauth2client.service_account import ServiceAccountCredentials
 
+from config.constants import SCOPE, JSON_ROUTE, MAIN_TABLE
+
+
+# open main table
+credentials = ServiceAccountCredentials.from_json_keyfile_name(JSON_ROUTE, SCOPE)
+client = gspread.authorize(credentials)
+table = client.open(MAIN_TABLE)
+
+
+# get programs DataFrame
+def get_programs_df():
+    return get_df('Programs')
+
+
+# get professors DataFrame
+def get_professors_df():
+    return get_df('Professors')
+
+
+# get managers DataFrame
+def get_managers_df():
+    return get_df('Managers')
+
+
+# get 5stars DataFrame
+def get_service_df():
+    return get_df('Five Stars')
+
+
+# get DataFrame from google sheet
+def get_df(sheet_name):
+    sheet = table.worksheet(sheet_name)
+
+    data = np.array(sheet.get_all_values())
+    records = data[1:]
+    columns = data[0]
+
+    df = pd.DataFrame(records, columns=columns)
+
+    return df
