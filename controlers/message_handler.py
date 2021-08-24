adcac91aa6dce51194a75e2fb154a7f31b6c273f
BRomanchuk <47035610+BRomanchuk@users.noreply.github.com>
2021-08-24 20:53:15 +0300

    upd message handlers

--- controlers/message_handler.py
+++ controlers/message_handler.py
@@ -1,10 +1,18 @@
+from telebot.types import ReplyKeyboardMarkup
+
 from core import school
 from data_loader import get_managers_df, get_programs_df, get_service_df, get_professors_df
 
+from settings.constants import START_MESSAGE
+
+
+# send start message
 def get_start_message(bot, message):
-    pass
+    start_message = "Привіт, " + str(message.from_user.first_name) + "!\n\n" + START_MESSAGE
+    bot.send_message(message.chat.id, start_message)
 
 
+# update all dataframes from google sheets
 def update_data(bot, message):
     school.programs_df = get_programs_df()
     school.professors_df = get_professors_df()
@@ -14,9 +22,16 @@ def update_data(bot, message):
     bot.send_message(message.chat.id, "✅ оновлено")
 
 
-def process_program_types(bot, message):
+# get programs of the certain type and make reply markup of them
+def process_program_type(bot, message):
     program_type = message.text[1:]
     programs = school.get_programs_by_type(program_type=program_type)
+
+    reply_markup = ReplyKeyboardMarkup(one_time_keyboard=True)
+    for program in programs:
+        reply_markup.add(program)
+
+    bot.send_message(message.chat.id, "Програми типу " + message.text + ":", reply_markup=reply_markup)
 
 
 def process_text(bot, message):
