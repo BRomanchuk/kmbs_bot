21ef7620378a51af823cda1aa84c7daae9775b34
BRomanchuk <47035610+BRomanchuk@users.noreply.github.com>
2021-08-25 15:23:46 +0300

    upd message handlers

--- controlers/message_handler.py
+++ controlers/message_handler.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 from telebot.types import ReplyKeyboardMarkup
+from telegram import ParseMode
 
 from core import school
 from data_loader import get_managers_df, get_programs_df, get_service_df, get_professors_df
@@ -59,9 +60,60 @@ def process_text(bot, message):
     :param message:
     :return:
     """
-    programs_and_staff = np.concat([school.get_programs(), school.get_programs(), school.get_managers(), school.get_five_stars()])
-    message_variants = get_message_variants(message, programs_and_staff)
 
+    school_entities = [
+        school.get_programs(),
+        school.get_professors(),
+        school.get_managers(),
+        school.get_five_stars()
+    ]
+
+    school_entities_dataframes = [
+        school.programs_df,
+        school.professors_df,
+        school.managers_df,
+        school.five_stars_df
+    ]
+
+    programs_and_staff_flat = np.concatenate([x for x in school_entities])
+    message_variants = get_message_variants(message, programs_and_staff_flat)
+
+    reply_markup = ReplyKeyboardMarkup(one_time_keyboard=True)
+    reply_message = ""
+
+    # for every variant of the message
     for msg in message_variants:
-        # TODO look at kmbs_tg
-        pass
+        # for every entity list in the school
+        for i in range(len(school_entities)):
+            indices_of_items = __get_indices_of_items(school_entities[i], msg)
+            if indices_of_items.shape[0] == 1:
+                reply_message = __construct_message_from_dataframe(school_entities_dataframes[i][indices_of_items[0]])
+                break
+            elif indices_of_items.shape[0] > 1:
+                reply_message = "Choose your fighter:"
+                for j in indices_of_items:
+                    reply_markup.row(school_entities[i][j])
+                break
+
+        if len(reply_message) > 0:
+            break
+
+    bot.send_message(message.chat.id, reply_message, reply_markup=reply_markup, parse_mode=ParseMode.HTML)
+
+# get indices of items in array that contain substr as a substring
+def __get_indices_of_items(arr, substr):
+    indices = np.array(arr.shape[0])
+    length_index = 0
+    for i in range(arr.shape[0]):
+        if substr in arr[i]:
+            indices[length_index] = i
+            length_index += 1
+    return indices[:length_index]
+
+
+# construct the message from the dataframe
+def __construct_message_from_dataframe(df):
+    message = ""
+    for col in df.columns:
+        message += "<b>" + col + ":</b>\n" + df[col] + "\n\n"
+    return message
